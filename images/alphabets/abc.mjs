6382cad8918ae8756f8a4fef99a8b78955f1d333
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2020-08-06 09:48:44 -0300

    Primeiro commit

---
+++ images/alphabets/abc.mjs
@@ -0,0 +1,35 @@
+<img src="[abc1] (importada)" width="455" height="455" border="0" usemap="#map" />
+
+<map name="map">
+<!-- #$-:Image map file created by GIMP Image Map plug-in -->
+<!-- #$-:GIMP Image Map plug-in by Maurits Rijk -->
+<!-- #$-:Please do not edit lines starting with "#$" -->
+<!-- #$VERSION:2.3 -->
+<!-- #$AUTHOR:Carlos - Tamanho padrão 76/75 -->
+<area shape="rect" coords="7,5,83,80"  nohref="nohref" />
+<area shape="rect" coords="98,5,174,80"  nohref="nohref" />
+<area shape="rect" coords="190,5,266,80"  nohref="nohref" />
+<area shape="rect" coords="277,5,353,80"  nohref="nohref" />
+<area shape="rect" coords="370,5,446,80"  nohref="nohref" />
+<area shape="rect" coords="7,80,83,155"  nohref="nohref" />
+<area shape="rect" coords="98,80,174,155"  nohref="nohref" />
+<area shape="rect" coords="190,80,266,155"  nohref="nohref" />
+<area shape="rect" coords="277,80,353,155"  nohref="nohref" />
+<area shape="rect" coords="370,80,446,155"  nohref="nohref" />
+<area shape="rect" coords="7,153,83,228"  nohref="nohref" />
+<area shape="rect" coords="98,153,174,228"  nohref="nohref" />
+<area shape="rect" coords="190,153,266,228"  nohref="nohref" />
+<area shape="rect" coords="277,153,353,228"  nohref="nohref" />
+<area shape="rect" coords="370,153,446,228"  nohref="nohref" />
+<area shape="rect" coords="7,227,83,302"  nohref="nohref" />
+<area shape="rect" coords="98,227,174,302"  nohref="nohref" />
+<area shape="rect" coords="277,227,353,302"  nohref="nohref" />
+<area shape="rect" coords="370,227,446,302"  nohref="nohref" />
+<area shape="rect" coords="7,301,83,376"  nohref="nohref" />
+<area shape="rect" coords="98,301,174,376"  nohref="nohref" />
+<area shape="rect" coords="277,301,353,376"  nohref="nohref" />
+<area shape="rect" coords="370,301,446,376"  nohref="nohref" />
+<area shape="rect" coords="190,227,266,302"  nohref="nohref" />
+<area shape="rect" coords="190,301,266,376" href="301" />
+<area shape="rect" coords="7,376,83,451"  nohref="nohref" />
+</map>
---
+++ images/cartoons/bobSponge/bobSponge.mjs
@@ -0,0 +1,14 @@
+<img src="[BobSponge] (importada)" width="900" height="230" border="0" usemap="#map" />
+
+<map name="map">
+<!-- #$-:Image map file created by GIMP Image Map plug-in -->
+<!-- #$-:GIMP Image Map plug-in by Maurits Rijk -->
+<!-- #$-:Please do not edit lines starting with "#$" -->
+<!-- #$VERSION:2.3 -->
+<!-- #$AUTHOR:Carlos -->
+<area shape="rect" coords="3,3,166,188" alt="Agradecendo"  nohref="nohref" />
+<area shape="rect" coords="176,2,373,212" alt="correndo"  nohref="nohref" />
+<area shape="rect" coords="387,2,557,228" alt="expectativa"  nohref="nohref" />
+<area shape="rect" coords="576,3,719,188" alt="perdendo"  nohref="nohref" />
+<area shape="rect" coords="732,1,896,194" alt="ganhando"  nohref="nohref" />
+</map>
