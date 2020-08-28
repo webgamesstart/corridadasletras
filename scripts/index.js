57932f4d51479aba40da41d253d6871f1bcd5fc8
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2020-08-28 11:06:51 -0300

    Acertando o maximo possivel sem testes

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-const cssElements = {
+const htmlElements = {
   root: document.querySelector(":root"),
 
   basicProtection: document.getElementById("basicProtection"),
 
-  splashScreen: document.getElementById("splashScreen"),
+  homeScreen: document.getElementById("homeScreen"),
 
   load: document.getElementById("load"),
   loading: document.querySelector("#load :nth-child(1)"),
@@ -22,8 +22,18 @@ const cssElements = {
 
   battleZone: document.getElementById("battleZone"),
 
-  challenge: document.getElementById("challenge"),
+  containerChallenge: document.getElementById("containerChallenge"),
   challengeImage: document.querySelector("#challenge img"),
+
+  speedWay: document.getElementById("speedWay"),
+  racersImgs: document.querySelectorAll("#speedWay div img"),
+  racerOne: document.getElementById("racerOne"),
+  racerTwo: document.getElementById("racerTwo"),
+
+  word: document.getElementById("word"),
+  wordSelected: document.querySelector("#word h2"),
+
+  board: document.getElementById("board"),
 };
 
 let speedLoad = [500, 1000, 3000, 300, 2000];
@@ -31,10 +41,13 @@ let flagLoad = 0;
 let controlLoader = null;
 
 let flagBreakPoint = Number(
-  getComputedStyle(cssElements.root).getPropertyValue("--breakPoint")
+  getComputedStyle(htmlElements.root).getPropertyValue("--breakPoint")
 );
 
 let flagLevel = "000";
+
+let filesTheLoad = [];
+let resourcesTheLoad = [];
 
 let flagCharacter = 0;
 
@@ -50,346 +63,234 @@ const challengesLevel000 = [
   "porco",
 ];
 
-const allCharacters = [
-  {
-    name: "Bob Esponja",
-    file: "spongeBob",
-    expectation: [
-      {
-        width: "110px",
-        height: "150px",
-        x: "-1px",
-        y: "-1x",
-      },
-      {
-        width: "110px",
-        height: "150px",
-        x: "-1px",
-        y: "-1px",
-      },
-      {
-        width: "110px",
-        height: "150px",
-        x: "-1px",
-        y: "-1px",
-      },
-    ],
-  },
-  {
-    name: "Patrick Estrela",
-    file: "starPatrick",
-    expectation: [
-      {
-        width: "93px",
-        height: "150px",
-        x: "-250px",
-        y: "-1px",
-      },
-      {
-        width: "93px",
-        height: "150px",
-        x: "-250px",
-        y: "-1px",
-      },
-      {
-        width: "93px",
-        height: "150px",
-        x: "-250px",
-        y: "-1px",
-      },
-    ],
-  },
-  {
-    name: "Homem Aranha",
-    file: "spiderMan",
-    expectation: [
-      {
-        width: "118px",
-        height: "150px",
-        x: "-490px",
-        y: "0px",
-      },
-      {
-        width: "118px",
-        height: "150px",
-        x: "-490px",
-        y: "0px",
-      },
-      {
-        width: "118px",
-        height: "150px",
-        x: "-490px",
-        y: "0px",
-      },
-    ],
-  },
-];
+// window.addEventListener("resize", function () {
+//   let temp = Number(
+//     getComputedStyle(htmlElements.root).getPropertyValue("--breakPoint")
+//   );
+//   if (flagBreakPoint != temp) {
+//     flagBreakPoint = Number(
+//       getComputedStyle(htmlElements.root).getPropertyValue("--breakPoint")
+//     );
+//     showCharacters();
+//   }
+// });
 
-window.addEventListener("resize", function () {
-  let temp = Number(
-    getComputedStyle(cssElements.root).getPropertyValue("--breakPoint")
-  );
-  if (flagBreakPoint != temp) {
-    flagBreakPoint = Number(
-      getComputedStyle(cssElements.root).getPropertyValue("--breakPoint")
-    );
-    showCharacters();
-  }
-});
-
-function load(resource, callback) {
+function load(resource, image, callback) {
   showLoad();
-  resource.src = "../images/cartoons/characters.png";
+  resource.src = image;
   resource.addEventListener("load", function () {
     hideLoad();
     callback();
   });
 }
 
-function hideSplashScreen() {
-  cssElements.splashScreen.style.display = "none";
+function loadMultipleFiles(resources, images, callback) {
+  showLoad();
+  let temp = 0;
+  for (let i = 0; i < resources.length; i++) {
+    resources[i].src = images[i];
+    resources[i].addEventListener("load", function () {
+      temp++;
+      if (temp >= resources.length) {
+        hideLoad();
+        callback();
+      }
+    });
+  }
+}
+
+function showHomeScreen() {
+  htmlElements.homeScreen.style.display = "block";
+}
+
+function hideHomeScreen() {
+  htmlElements.homeScreen.style.display = "none";
 }
 
 function showSelectPlayer() {
-  cssElements.panelSelectPlayer.style.display = "flex";
-  cssElements.panelSelectPlayerTitle.style.display = "flex";
-  cssElements.SelectPlayerTitle.style.display = "block";
-  cssElements.selectPlayer.style.display = "flex";
-  cssElements.selectPlayerPrevious.style.display = "block";
-  cssElements.selectPlayerNext.style.display = "block";
-  cssElements.characters.style.display = "block";
-  cssElements.charactersImg.style.display = "block";
+  htmlElements.panelSelectPlayer.style.display = "flex";
+  htmlElements.panelSelectPlayerTitle.style.display = "flex";
+  htmlElements.SelectPlayerTitle.style.display = "block";
+  htmlElements.selectPlayer.style.display = "flex";
+  htmlElements.selectPlayerPrevious.style.display = "block";
+  htmlElements.selectPlayerNext.style.display = "block";
+  htmlElements.characters.style.display = "block";
+  htmlElements.charactersImg.style.display = "block";
   showCharacters();
 }
 
 function hideSelectPlayer() {
-  cssElements.panelSelectPlayer.style.display = "none";
-  cssElements.panelSelectPlayerTitle.style.display = "none";
-  cssElements.SelectPlayerTitle.style.display = "none";
-  cssElements.selectPlayer.style.display = "none";
-  cssElements.selectPlayerPrevious.style.display = "none";
-  cssElements.selectPlayerNext.style.display = "none";
-  cssElements.characters.style.display = "none";
-  cssElements.charactersImg.style.display = "none";
+  htmlElements.panelSelectPlayer.style.display = "none";
+  htmlElements.panelSelectPlayerTitle.style.display = "none";
+  htmlElements.SelectPlayerTitle.style.display = "none";
+  htmlElements.selectPlayer.style.display = "none";
+  htmlElements.selectPlayerPrevious.style.display = "none";
+  htmlElements.selectPlayerNext.style.display = "none";
+  htmlElements.characters.style.display = "none";
+  htmlElements.charactersImg.style.display = "none";
 }
 
 function showCharacters() {
-  cssElements.root.style.setProperty(
+  htmlElements.root.style.setProperty(
     "--characterWidth",
     allCharacters[flagCharacter].expectation[flagBreakPoint].width
   );
-  cssElements.root.style.setProperty(
+  htmlElements.root.style.setProperty(
     "--characterHeight",
     allCharacters[flagCharacter].expectation[flagBreakPoint].height
   );
-  cssElements.charactersImg.style.left =
+  htmlElements.charactersImg.style.left =
     allCharacters[flagCharacter].expectation[flagBreakPoint].x;
-  cssElements.charactersImg.style.top =
+  htmlElements.charactersImg.style.top =
     allCharacters[flagCharacter].expectation[flagBreakPoint].y;
 }
 
+function showCharactersThanking() {
+  htmlElements.root.style.setProperty(
+    "--characterWidth",
+    allCharacters[flagCharacter].thanking.width
+  );
+  htmlElements.root.style.setProperty(
+    "--characterHeight",
+    allCharacters[flagCharacter].thanking.height
+  );
+  htmlElements.charactersImg.style.left =
+    allCharacters[flagCharacter].thanking.x;
+  htmlElements.charactersImg.style.top =
+    allCharacters[flagCharacter].thanking.y;
+  setTimeout(showBattleZone, 1000);
+}
+
 function showBasicProtection() {
-  cssElements.basicProtection.style.display = "block";
+  htmlElements.basicProtection.style.display = "block";
 }
 function hideBasicProtection() {
-  cssElements.basicProtection.style.display = "none";
+  htmlElements.basicProtection.style.display = "none";
 }
 
 function showLoad() {
-  cssElements.load.style.display = "flex";
-  cssElements.loading.style.display = "block";
-  cssElements.loadLevel.style.display = "block";
-  cssElements.loadLevelOne.style.display = "block";
-  cssElements.loadLevelTwo.style.display = "block";
+  htmlElements.load.style.display = "flex";
+  htmlElements.loading.style.display = "block";
+  htmlElements.loadLevel.style.display = "block";
+  htmlElements.loadLevelOne.style.display = "block";
+  htmlElements.loadLevelTwo.style.display = "block";
   showLoader();
 }
 
 function showLoader() {
   flagLoad = Math.floor(Math.random() * speedLoad.length);
-  cssElements.root.style.setProperty("--loadSpeed", `${speedLoad[flagLoad]}ms`);
+  htmlElements.root.style.setProperty(
+    "--loadSpeed",
+    `${speedLoad[flagLoad]}ms`
+  );
   controlLoader = setTimeout(showLoader, `${speedLoad[flagLoad]}`);
 }
 
 function hideLoad() {
-  cssElements.load.style.display = "none";
-  cssElements.loading.style.display = "none";
-  cssElements.loadLevel.style.display = "none";
-  cssElements.loadLevelOne.style.display = "none";
-  cssElements.loadLevelTwo.style.display = "none";
+  htmlElements.load.style.display = "none";
+  htmlElements.loading.style.display = "none";
+  htmlElements.loadLevel.style.display = "none";
+  htmlElements.loadLevelOne.style.display = "none";
+  htmlElements.loadLevelTwo.style.display = "none";
   clearInterval(controlLoader);
+}
+
+function showChallenge() {
+  htmlElements.challenge.style.display = "block";
+  htmlElements.challengeImage.style.display = "block";
+  showChallengeSelected();
+}
+function hideChallenge() {
+  htmlElements.challenge.style.display = "none";
+  htmlElements.challengeImage.style.display = "none";
 }
 
 function selectChallenge() {
   let level = eval(`challengesLevel${flagLevel}`);
   let random = Math.floor(Math.random() * level.length);
   let temp = `./images/challenges/level${flagLevel}/${level[random]}.png`;
-  cssElements.challengeImage.setAttribute("src", temp);
+  htmlElements.challengeImage.setAttribute("src", temp);
+}
+
+function showChallengeSelected() {
+  // let temp = selectChallenge();
+  let temp = challengesLevel000[0][1];
+  console.log(temp.nome);
+  htmlElements.challenge.style.width = temp.width;
+  htmlElements.challengeImage.style.left = temp.x;
+}
+
+function selectFileChallenge() {
+  let level = eval(`challengesLevel${flagLevel}`);
+  if (flagFileForLevel <= level.length) {
+    fileChallengeTheLoad = `../images/challenges/level${flagLevel}/${flagFileForLevel}.png`;
+  }
+}
+
+function loadChallenge() {
+  selectFileChallenge();
+  load(htmlElements.challengeImage, fileChallengeTheLoad, showChallenge);
 }
 
 function showBattleZone() {
-  cssElements.battleZone.style.display = "grid";
-  showChallenge();
+  hideSelectPlayer();
+  htmlElements.battleZone.style.display = "grid";
+  htmlElements.containerChallenge.style.display = "flex";
+  htmlElements.speedWay.style.display = "flex";
+  htmlElements.word.style.display = "flex";
+  htmlElements.board.style.display = "flex";
+  loadChallenge();
+  showRacers();
 }
 
-function showChallenge() {
-  cssElements.challenge.style.display = "flex";
-  cssElements.challengeImage.style.display = "block";
-}
-
-splashScreen.addEventListener("click", function () {
-  hideSplashScreen();
-  load(cssElements.charactersImg, showSelectPlayer);
+homeScreen.addEventListener("click", function () {
+  hideHomeScreen();
+  load(htmlElements.charactersImg, showSelectPlayer);
 });
 
-cssElements.selectPlayerNext.addEventListener("click", function () {
+htmlElements.selectPlayerNext.addEventListener("click", function () {
   if (flagCharacter < allCharacters.length - 1) {
     flagCharacter++;
     showCharacters();
   }
 });
 
-cssElements.selectPlayerPrevious.addEventListener("click", function () {
+htmlElements.selectPlayerPrevious.addEventListener("click", function () {
   if (flagCharacter >= 1) {
     flagCharacter--;
     showCharacters();
   }
 });
 
-// const root = document.querySelector(":root");
+htmlElements.panelSelectPlayerOK.addEventListener(
+  "click",
+  showCharactersThanking
+);
 
-// const characters = document.getElementById("characters");
-// const character = document.querySelector("#characters img");
-// const characterPrevious = document.querySelector("#selectPlayer #previous");
-// const characterNext = document.querySelector("#selectPlayer #next");
+function showRacers() {
+  htmlElements.racerOne.style.display = "block";
+  htmlElements.racerTwo.style.display = "block";
+}
 
-// const challengeImage = document.querySelector("#challenge img");
+function hideRacers() {
+  htmlElements.racerOne.style.display = "none";
+  htmlElements.racerTwo.style.display = "none";
+}
 
-// const racer1 = document.getElementById("one");
-// const racer2 = document.getElementById("two");
+function selectRacers() {
+  competitors[0] = allCharacters[flagCharacter].file;
+  let indexSelected = Math.floor(Math.random() * allCharacters.length);
+  while (flagCharacter === indexSelected) {
+    indexSelected = Math.floor(Math.random() * allCharacters.length);
+  }
+  competitors[1] = allCharacters[indexSelected].file;
+}
 
-// const bobSponge = {
-//   agradecendo:{
-//     x: '3',
-//     y: '3',
-//     scale: ''
-//   },
-//   correndo:{
-//     top: '85%',
-//     width: '85px',
-//     height: '90px',
-//     x: '-335px',
-//     y: '-68px',
-//     scale: '0.4'
-//   },
-//   expectativa:{
-//     x: '387',
-//     y: '2',
-//     scale: ''
-//   },
-//   perdendo:{
-//     x: '576',
-//     y: '3',
-//     scale: ''
-//   },
-//   ganhando:{
-//     x: '732',
-//     y: '1',
-//     scale: ''
-//   }
-// };
+function positionCharacters() {
+  selectRacers();
+  racer1.src = `./images/cartoons/${competitors[0]}/${competitors[0]}.png`;
+  // racer1.style.transform = `translateX(${bobSponge.correndo.x}) translateY(${bobSponge.correndo.y}) scale(${bobSponge.correndo.scale},${bobSponge.correndo.scale})`;
 
-// const patrickStar = {
-//   agradecendo:{
-//     x: '3',
-//     y: '3',
-//     scale: ''
-//   },
-//   correndo:{
-//     top: '85%',
-//     width: '85px',
-//     height: '90px',
-//     x: '-300px',
-//     y: '-80px',
-//     scale: '0.35'
-//   },
-//   expectativa:{
-//     x: '387',
-//     y: '2',
-//     scale: ''
-//   },
-//   perdendo:{
-//     x: '576',
-//     y: '3',
-//     scale: ''
-//   },
-//   ganhando:{
-//     x: '732',
-//     y: '1',
-//     scale: ''
-//   }
-// };
-
-// const challengesLevel000 = [
-//   "abelha",
-//   "cachorro",
-//   "gato",
-//   "girafa",
-//   "leao",
-//   "pato",
-//   "porco",
-// ];
-
-// root.style.setProperty("--characterWidth", allCharacters[flagCharacter].width);
-// character.src = "../images/cartoons/characters.png";
-// character.style.transform = `translateX(${allCharacters[flagCharacter].x}) translateY(${allCharacters[flagCharacter].y}) scale(${allCharacters[flagCharacter].scale},${allCharacters[flagCharacter].scale})`;
-
-// function selectRacers(){
-//   competitors[0] = allCharacters[flagCharacter].file;
-//   let indexSelected = Math.floor(Math.random() * allCharacters.length);
-//   while(flagCharacter === indexSelected){
-//     indexSelected = Math.floor(Math.random() * allCharacters.length);
-//   };
-//   competitors[1] = allCharacters[indexSelected].file;
-// };
-
-// characterNext.addEventListener("click", function () {
-//   if (flagCharacter < allCharacters.length - 1) {
-//     flagCharacter++;
-//     root.style.setProperty(
-//       "--characterWidth",
-//       allCharacters[flagCharacter].width
-//     );
-//     character.style.transform = `translateX(${allCharacters[flagCharacter].x}) translateY(${allCharacters[flagCharacter].y}) scale(${allCharacters[flagCharacter].scale},${allCharacters[flagCharacter].scale})`;
-//   }
-// });
-
-// characterPrevious.addEventListener("click", function () {
-//   if (flagCharacter >= 1) {
-//     flagCharacter--;
-//     root.style.setProperty(
-//       "--characterWidth",
-//       allCharacters[flagCharacter].width
-//     );
-//     character.style.transform = `translateX(${allCharacters[flagCharacter].x}) translateY(${allCharacters[flagCharacter].y}) scale(${allCharacters[flagCharacter].scale},${allCharacters[flagCharacter].scale})`;
-//   }
-// });
-
-// function selectChallenge() {
-//   let level = eval(`challengesLevel${flagLevel}`);
-//   let random = Math.floor(Math.random() * level.length);
-//   let temp = `./images/challenges/level${flagLevel}/${level[random]}.png`;
-//   challengeImage.setAttribute("src", temp);
-// }
-
-// function positionCharacters() {
-//   selectRacers();
-//   racer1.src = `./images/cartoons/${competitors[0]}/${competitors[0]}.png`;
-//   // racer1.style.transform = `translateX(${bobSponge.correndo.x}) translateY(${bobSponge.correndo.y}) scale(${bobSponge.correndo.scale},${bobSponge.correndo.scale})`;
-
-//   racer2.src = `./images/cartoons/${competitors[1]}/${competitors[1]}.png`;
-//   // racer2.style.transform = `translateX(${patrickStar.correndo.x}) translateY(${patrickStar.correndo.y}) scale(${patrickStar.correndo.scale},${patrickStar.correndo.scale})`;
-// }
-
-// selectChallenge();
-
-// positionCharacters();
+  racer2.src = `./images/cartoons/${competitors[1]}/${competitors[1]}.png`;
+  // racer2.style.transform = `translateX(${patrickStar.correndo.x}) translateY(${patrickStar.correndo.y}) scale(${patrickStar.correndo.scale},${patrickStar.correndo.scale})`;
+}
